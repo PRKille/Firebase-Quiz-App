dc5868fe4874b9665d94bace586e12f61250165d
Patrick Kille <pr.kille@gmail.com>
2020-05-04 10:31:33 -0700

    create new survey form
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Geoff Goetz <59552871+Pieharder@users.noreply.github.com>

---
+++ src/components/NewSurveyForm.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NewSurveyForm(props) {
+
+  function handleNewSurveySubmission(event) {
+    event.preventDefault();
+    props.onNewSurveySubmission({
+      title: event.target.title.value,
+      instructions: event.target.instructions.value,
+      question1: event.target.question1.value,
+      question2: event.target.question2.value,
+      question3: event.target.question3.value,
+      question4: event.target.question4.value,
+      question5: event.target.question5.value,
+      question6: event.target.question6.value
+    });
+  };
+
+  return (
+    <form onSubmit={this.handleNewSurveySubmission}>
+      <input
+        type="text"
+        name="title"
+        placeholder="title of quiz" required />
+      <textarea
+        name="instructions"
+        placeholder="enter any instructions or background about the quiz that a user should know" required />
+      <input 
+        type="text"
+        name="question1"
+        placeholder="on a scale of one to five, ..." required />
+      <input 
+        type="text"
+        name="question2"
+        placeholder="on a scale of one to five, ..." required />
+      <input 
+        type="text"
+        name="question3"
+        placeholder="on a scale of one to five, ..." required />
+      <input 
+        type="text"
+        name="question4"
+        placeholder="on a scale of one to five, ..." required />
+      <input 
+        type="text"
+        name="question5"
+        placeholder="on a scale of one to five, ..." required />
+      <input
+        type="text"
+        name="question6"
+        placeholder="write a question that requires a text response, such as requesting feedback." required />
+      <button type="submit">create your survey!</button>
+    </form>
+  );
+}
+
+NewSurveyForm.propTypes = {
+  onNewSurveySubmission: PropTypes.func
+}
+
+export default NewSurveyForm;

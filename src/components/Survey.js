2ead2d431c84d378759c1c6c1c2fcccde80c4ceb
Patrick Kille <pr.kille@gmail.com>
2020-05-04 11:16:02 -0700

    create survey component
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Geoff Goetz <59552871+Pieharder@users.noreply.github.com>

---
+++ src/components/Survey.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Survey(props) {
+  return (
+    <div onClick={props.whenSurveyClicked(props.id)}>
+      <h3>{props.title}</h3>
+    </div>
+  );
+}
+
+Survey.propTypes = {
+  title: PropTypes.string,
+  id: PropTypes.string,
+  key: PropTypes.string,
+  whenSurveyClicked: PropTypes.func
+}
+
+export default Survey;
